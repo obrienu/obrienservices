bf998cdc6dfcb016bd4896ed97da7e2dee07d4a4
Obrien Longe <obrienules@gmail.com>
2022-01-14 21:33:57 +0100

    microservice set up with customer

---
+++ customer/src/main/java/com/obrienservice/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,5 @@
+package com.obrienservice.customer;
+
+public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+
+}
---
+++ customer/src/main/java/com/obrienservice/customer/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.obrienservice.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
